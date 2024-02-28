998279bce4113abfaf60a168d92213afaaa8a9a4
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2024-02-28 22:53:55 +0000

    Successfully disabled canvas scrolling

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,13 +1,20 @@
 // app/javascript/components/App.jsx
-import React, { useEffect } from "react";
-import { Tldraw } from "@tldraw/tldraw";
-
-import "@tldraw/tldraw/tldraw.css";
+import React, { useEffect, useRef, useState } from 'react';
+import { Editor, Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css';
 
 export default function App() {
+  const [focused, setFocused] = useState(false);
+  const rEditorRef = (useRef < Editor) | (null > null);
+
+  useEffect(() => {
+    const editor = rEditorRef.current;
+    if (!editor) return;
+    editor.updateInstanceState({ isFocused: false });
+  }, [focused]);
   return (
-    <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw editor />
+    <div style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw autoFocus={false} />
     </div>
   );
 }
